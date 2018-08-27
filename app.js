0bcf076540310280c1d0595ec97798786999a56f
Abey Onalaja <abey.onalaja@gmail.com>
2018-08-27 23:10:29 +0100

    Set up config for passport

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var fs = require("fs"),
   mongoose = require("mongoose");
 
 require("./models/User");
+require("./config/passport");
 
 var isProduction = process.env.NODE_ENV === "production";
 
